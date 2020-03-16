22d5c426b20b90132e2afe374b94b9cfc8568d68
xle6 <62257072+xle6@users.noreply.github.com>
2020-03-16 18:56:15 +0200

    Create min.js

---
+++ min.js
@@ -0,0 +1,10 @@
+function theme() {
+    
+    document.getElementById("theme").style.background ="white";//xle68
+    document.getElementById("font").style.color ="black"; //xle68
+    document.body.style.background ="black";//xle68
+    document.getElementById("font").innerText ="White Theme";//xle68
+    window.alert('DarK Theme By Xle68');//xle68
+
+}
+//xle68
